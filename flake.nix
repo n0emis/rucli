537241bc5de4149dfa8f1efcb5512f247a9a52b8
Ember Keske <git@n0emis.eu>
2023-07-13 13:11:45 +0200

    convert to flakes

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "rucli";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlays.default = final: prev: {
+      rucli = final.callPackage (
+        { lib, stdenv, rustPlatform, pkg-config, openssl }:
+
+        rustPlatform.buildRustPackage {
+          pname = "rucli";
+          version =
+            self.shortRev or "dirty-${toString self.lastModifiedDate}";
+          src = self;
+
+          cargoBuildFlags = lib.optionals (stdenv.hostPlatform.isMusl && stdenv.hostPlatform.isStatic) [ "--features" "mimalloc" ];
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ openssl ];
+        }
+      ) { };
+    };
+  } // flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ self.overlays.default ];
+    };
+  in rec {
+    packages = {
+      inherit (pkgs) rucli;
+      default = packages.rucli;
+    };
+    legacyPackages = pkgs;
+  });
+}
